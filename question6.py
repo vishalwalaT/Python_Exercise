37e40db4603df199b8f3a1176df05d213b8592aa
Vishal Wala <vishalw.tagline@gmail.com>
2023-09-01 11:04:54 +0530

    Question6 6.2 commit'changed variable name'

--- question6.py
+++ question6.py
@@ -2,9 +2,9 @@ string = "PQRQRQRQ"
 substring = "QRQ"
 
 count = 0
-lgth = len(string)
-lgth_substr = len(substring)
-for i in range(0, lgth):
-    if substring == string[i : i + lgth_substr]:
+str_length = len(string)
+substr_length = len(substring)
+for i in range(0, str_length):
+    if substring == string[i : i + substr_length]:
         count += 1
 print(count)
